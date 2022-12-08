fb1942e62c4a82e64b0a4cb8e2dfcab290bd9c46
jarolthecoder <jarolr.webdev@gmail.com>
2022-12-08 08:46:04 -0500

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -9,15 +9,27 @@ scrollBtn.addEventListener('click', ()=> {
 const mobileNav = document.querySelector('#mobile-navbar');
 const mobileToggleBtn = document.querySelector('.nav-toggle');
 const toggleBar = document.querySelectorAll('.bar');
+const navigation = document.querySelector('.navigation-mobile');
+const linkMobile = document.querySelectorAll('.navigation-mobile .nav-item');
+const contactNav = document.querySelector('.contact-info-mobile');
+const socialLinkBlock = document.querySelector('.social-media-mobile');
 
 mobileToggleBtn.addEventListener('click', () => {
   if(mobileNav.classList.contains('nav-active')) {
 			mobileNav.classList.remove('nav-active');
+      navigation.classList.remove('fade-up');
+      contactNav.classList.remove('fade-up');
+			socialLinkBlock.classList.remove('fade-up');
 			toggleBar.forEach((b)=> b.classList.remove('bar-active'));
+      linkMobile.forEach((l)=> l.classList.remove('fade-up'));
       document.body.style.position = 'static';
 		} else {
 			mobileNav.classList.toggle('nav-active');
-			toggleBar.forEach((b)=> b.classList.toggle('bar-active'));
+      navigation.classList.add('fade-up')
+			contactNav.classList.add('fade-up');
+			socialLinkBlock.classList.add('fade-up');
+      toggleBar.forEach((b)=> b.classList.add('bar-active'));
+      linkMobile.forEach((l)=> l.classList.add('fade-up'));
       document.body.style.position = 'fixed';
       document.body.style.width = '100%';
 		}
